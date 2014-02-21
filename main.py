055caba2c155c70837e0d1d53b0ed5cf9698864d
Sarma Tangirala <vtangira@buffalo.edu>
2014-02-21 14:33:26 -0500

    Bug fix.

--- main.py
+++ main.py
@@ -48,14 +48,14 @@ class SampleStreamListener(StreamListener):
         if self.count < 100:
             tweet = json.loads(data)
             if 'text' in tweet:
-                print 'Getting Tweet #'+str(self.count)
-                sample = open('twitter_sample.txt', 'w')
+                print 'Getting Tweet #'+str(self.count+1)
+                sample = open('twitter_sample.txt', 'a')
                 #sample.write(tweet['text'].encode('utf-8')+'\n')
                 sample.write(json.dumps(tweet)+'\n')
                 self.count += 1
                 sample.close()
-            else:
-                exit(0)
+        else:
+            exit(0)
         return True
 
     def on_error(self, status):
@@ -68,20 +68,12 @@ def sampleTwitter():
     config = ConfigParser.ConfigParser()
     config.read('conf/config.file')
     section = 'Twitter'
-    options = config.options(section)
-    configdict = {}
-    for option in options:
-        configdict[option] = config.get(section, option)
     key_list = ['consumer_key', 'consumer_secret', 'token_key', 'token_secret']
-    for key in key_list:
-        if key not in configdict:
-            print 'Bad config file.'
-            return(-1)
-
     con_key = config.get(section, 'consumer_key')
     con_secret = config.get(section, 'consumer_secret')
     token_key = config.get(section, 'token_key')
     token_secret = config.get(section, 'token_secret')
+
     listener = SampleStreamListener()
     auth = OAuthHandler(con_key, con_secret)
     auth.set_access_token(token_key, token_secret)
@@ -109,6 +101,7 @@ if __name__ == '__main__':
     for line in sample:
         tweetjson = json.loads(line)
         print tweetjson['Text']
+    sample.close()
 
 
     ''' Return sample sentiment. '''
