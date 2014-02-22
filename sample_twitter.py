114d0d404ed43ca318d41f5f1f7ef57db04f0b07
Sarma Tangirala <vtangira@buffalo.edu>
2014-02-22 03:58:51 -0500

    Adding simple emoticon sentiment using regexp.

--- sample_twitter.py
+++ sample_twitter.py
@@ -3,8 +3,42 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import ConfigParser
 import json
+import re
 
-def clean_text(text):
+def baseSentiment(text):
+
+    ''' Adapted from https://github.com/aritter/twitter_nlp/blob/master/python/emoticons.py
+        Regexps to handle emoticons. '''
+
+    mycompile = lambda pat:  re.compile(pat,  re.UNICODE)
+
+    NormalEyes = r'[;:=]'
+
+    NoseArea = r'(|o|O|-)'   ## rather tight precision, \S might be reasonable...
+
+    HappyMouths = r'[}pPD\)\]]'
+    SadMouths = r'[{\(\[]'
+    OtherMouths = r'[doO/\\]'  # remove forward slash if http://'s aren't cleaned
+
+    Happy_RE =  mycompile( '(\^_\^|' + NormalEyes + NoseArea + HappyMouths + ')')
+    Sad_RE = mycompile(NormalEyes + NoseArea + SadMouths)
+    Surprise_RE = mycompile(NormalEyes + NoseArea + OtherMouths)
+
+    #Emoticon = ("("+NormalEyes+"|"+Wink+")" + NoseArea + "("+Tongue+"|"+OtherMouths+"|"+SadMouths+"|"+HappyMouths+")")
+    #Emoticon_RE = mycompile(Emoticon)
+
+    h = Happy_RE.search(text)
+    s = Sad_RE.search(text)
+    n = Surprise_RE.search(text)
+    if h and s: return "BOTH_HS"
+    if h: return "HAPPY"
+    if s: return "SAD"
+    if n: return "SURPRISE"
+    return "NA"
+
+def cleanText(text):
+
+    ''' Removes RT, Mentions and urls. '''
 
     check_string = 'abcdefghijklmnopqrstuvwxyz'
 
@@ -30,7 +64,7 @@ class SampleStreamListener(StreamListener):
             tweet = json.loads(data)
             if 'text' in tweet:
                 text = tweet['text']
-                clean_tokens = clean_text(text)
+                clean_tokens = cleanText(text)
                 if len(clean_tokens) > 0:
                     print 'Getting Tweet #'+str(self.count+1)
                     sample = open('twitter_sample.txt', 'a')
